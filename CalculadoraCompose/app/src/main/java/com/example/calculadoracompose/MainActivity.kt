cc9523ffaa081cffadda7bdcd9ff94217e20508c
unknown <juliocesar.silvadi@elorrieta-errekamari.com>
2024-09-22 22:19:56 +0200

    Configuracion del textField
    -el texto empieza por la derecha
    -altura
    -color

--- CalculadoraCompose/app/src/main/java/com/example/calculadoracompose/MainActivity.kt
+++ CalculadoraCompose/app/src/main/java/com/example/calculadoracompose/MainActivity.kt
@@ -15,20 +15,28 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -57,19 +65,43 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun calculadoraInterface() {
     val tamanioFuentaboton = 18.sp
+    var text by remember { mutableStateOf(TextFieldValue("")) }
 
-    Column(modifier = Modifier.fillMaxHeight()) {
+    Column(modifier = Modifier.fillMaxHeight()
+        .background(androidx.compose.ui.graphics.Color.White)) {
         Row(
             modifier = Modifier.fillMaxWidth()
         ) {
             TextField(
-                modifier = Modifier.weight(1f).padding(bottom = 30.dp, top = 10.dp),
-                value = "",
-                onValueChange={},
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = 150.dp, top = 130.dp)
+                    .height(150.dp),
+                value = text,
+                onValueChange = {
+                    text = it
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Text
+                ),
+                textStyle = TextStyle(
+                    textAlign = TextAlign.End,
+                    fontSize = 24.sp,
+                    color = androidx.compose.ui.graphics.Color.Black
+                ),
+
+
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = androidx.compose.ui.graphics.Color.Transparent,
+                    unfocusedIndicatorColor = androidx.compose.ui.graphics.Color.Transparent,
+                    focusedIndicatorColor = androidx.compose.ui.graphics.Color.Transparent
+                )
+
             )
         }
         Row(
